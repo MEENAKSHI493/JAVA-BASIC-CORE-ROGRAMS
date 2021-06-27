5ffa27e80d9cb4d9dd078df5dfd56d1f5e1a4d04
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-27 18:51:22 +0530

    harmonic number

--- src/com/Bridgelabz/Harmonicnumber.java
+++ src/com/Bridgelabz/Harmonicnumber.java
@@ -1,12 +1,22 @@
 package com.Bridgelabz;
 import java.util.Scanner;
 public class Harmonicnumber {
+	public static void main(String[] args) 
+    {
+        System.out.print("Enter any number : ");
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        System.out.print("The Harmonic Series is : ");
+        double result = 0.0;
+        while(num > 0)
+          {
+               result = result + (double) 1 / num;
+               num--;
+               System.out.print(result +"  ");
+          }
+        System.out.println("");
+        System.out.println("Output of Harmonic Series is "+result);
+    }
 
-	Scanner scan = new Scanner(System.in);
-	
-	}
-		
-		
 	
 }
-	}
